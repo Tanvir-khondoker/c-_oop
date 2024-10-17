104d6b2d13c4882cc5bccf0a22d39a0c1abe19bc
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2024-10-17 12:56:30 +0600

    first commit

---
+++ class.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+// here teacher is a blueprint or object
+class Teacher{
+    //properties or attributes
+    string name;
+    string dept;
+    string subject;
+    float salary;
+
+    // methods/member function
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+};
+
+int main()
+{
+
+   Teacher t1;
+   t.
+
+    return 0;
+}
