7b4a05578aaaa3cd45d5fc4ed5afe2b59a9edde0
Md.Tanvir-Hossain-khondoker <tanvirkhondo21@gmail.com>
2024-10-17 14:48:36 +0600

    class implemented

--- class.cpp
+++ class.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 // here teacher is a blueprint or object
 class Teacher{
+    public:
     //properties or attributes
     string name;
     string dept;
@@ -20,7 +21,13 @@ int main()
 {
 
    Teacher t1;
-   t.
+   t1.name = "David John";
+   t1.dept = "Computer Science";
+   t1.subject = "c++";
+   t1.salary = 40000.42;
+//    t1.changeDept
+
+  cout<<t1.name<<endl;
 
     return 0;
 }
